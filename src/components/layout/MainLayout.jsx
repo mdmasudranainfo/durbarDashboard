1fe5d3acd0283e490a2793b4bccc405caa6d4424
mdmasudranainfo <mdmasude7@gmail.com>
2024-05-06 15:44:54 +0600

    side nav

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import Navbar from "../share/navbar/Navbar";
 import Sidebar from "../share/sidebar/Sidebar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 const MainLayout = () => {
   const [toggle, setToggle] = useState(false);
+
+  const location = useLocation();
+
+  const pathName1 = location.pathname.split("/")[1];
+  const pathName2 = location.pathname.split("/")[1];
   return (
     <>
       <Navbar setToggle={setToggle} toggle={toggle} />
@@ -22,6 +27,12 @@ const MainLayout = () => {
 
         {/* content */}
         <div className=" w-full overflow-y-auto h-[90vh] sidebarScroll  ">
+          <div className="bg-dark-blue-50 flex items-center justify-between mx-8 p-4 mb-4">
+            <h1 className="uppercase font-bold">{pathName1}</h1>
+            <div className="flex gap-1">
+              <span>{pathName1}</span>/<span>{pathName2}</span>
+            </div>
+          </div>
           <Outlet />
         </div>
       </div>
